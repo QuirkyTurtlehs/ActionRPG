e02433a34fccd8aa33f8097af8d31319636e23a5
QuirkyTurtlehs <38839218+QuirkyTurtlehs@users.noreply.github.com>
2019-11-03 02:33:04 +0100

    First commit
    :)

---
+++ ActionRPG/Assets/Game/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame updat
+    public GameObject target;
+
+    [SerializeField] float smoothTime = 0.5f;
+
+    [SerializeField] Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            FollowTarget(target);
+        }
+    }
+
+    void FollowTarget(GameObject target)
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position, ref velocity,smoothTime);
+
+    }
+}
---
+++ ActionRPG/Assets/Game/Scripts/Player/TempPlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TempPlayerMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Rigidbody rb;
+
+    public Camera mainCamera;
+
+    float moveSpeed;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //rb = GetComponent<Rigidbody>();
+    }   
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseButtonDown();
+        }
+    }
+    void OnMouseButtonDown()
+    {
+        MovePlayer();
+    }       
+    void MovePlayer()
+    {
+        RaycastHit hit;
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }       
+    }
+}
